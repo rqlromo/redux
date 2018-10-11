8d3d9e610f251df741c089e9fa17f0b41d85ec25
Raquel Romo <rqlromo@gmail.com>
2018-10-11 18:27:15 +0200

    scafolding react with redux

---
+++ src/reducers/root.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+
+// import contacts from './contacts'
+
+
+//genera el store general
+const rootReducer = combineReducers({
+
+})
+
+export default rootReducer
