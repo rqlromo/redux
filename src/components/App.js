445bf3a1620b316ba2f2b8ec041b1adba869b72a
Raquel Romo <rqlromo@gmail.com>
2018-10-17 18:46:45 +0200

    create components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 // import logo from '../logo.svg';
 import '../App.css';
+import ProductList from './ProductList';
+import SecondaryProductList from './SecondaryProductList';
 
 class App extends Component {
   render() {
     return (
-      <div className="">
-
+      <div className="container-market">
+        <header>
+          <h1>
+            MarketPlace chachi Pistachi
+          </h1>
+        </header>
+        <div className="subcontainer-market">
+          <ProductList/>
+          <SecondaryProductList/>
+          <SecondaryProductList/>
+        </div>
       </div>
     );
   }
---
+++ src/components/ProductList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className="container-list">
+        <h3>
+          ProductList
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
